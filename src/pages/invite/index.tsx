4c1d17269f78f066465a6c6b55333b6d2e47a546
dxiaoqi <1035038018@qq.com>
2022-11-09 23:48:57 +0800

    feat：ui

--- src/pages/invite/index.tsx
+++ src/pages/invite/index.tsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 import querystring from 'query-string'
 import styles from './index.module.scss';
 import InviteAvatar from '../../assert/invite_avatar.png';
+import Avatar from '../../assert/invite-avatar.png';
 export default function Invite () {
   const search = querystring.parse(window.location.href.split('?')[1]);
   const cid = search.cid;
@@ -11,7 +12,13 @@ export default function Invite () {
   })
   return (
     <div className={styles.container}>
-    <img src={InviteAvatar} alt="" />
+      <div className={styles.header}>
+        <div className={styles.avatar}>
+          <img src={InviteAvatar} alt="" />
+          <img src={Avatar} alt="" />
+        </div>
+      </div>
+      <p className={styles.address}>{13232321313123}</p>
     <p className={styles.tip}>
     You can click the invite button to invite 2 people to the conference
     </p>
